fe0abe553a8484d4a09f3dc3d95d4b67d8c76925
Hirochon <heacet43@gmail.com>
2022-01-04 01:31:40 +0900

    add: calling db

--- interface/restapi/unko.go
+++ interface/restapi/unko.go
@@ -1,15 +1,34 @@
 package restapi
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+
+	sqlc "github.com/Hirochon/practice-sqlc/infra/gen/sqlc"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func Unko(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
+		ctx := r.Context()
+
+		db, err := sql.Open("mysql", "noda:nodanoda@(mysql:3306)/nodasql?parseTime=true&autocommit=0")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		queries := sqlc.New(db)
+
+		allUnko, err := queries.GetAllUnko(ctx)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("%+v¥n", allUnko)
+
 		w.WriteHeader(http.StatusOK)
-		_, err := fmt.Fprintln(w, "うんこ OK!!!")
+		_, err = fmt.Fprintln(w, "うんこ OK!!!")
 		if err != nil {
 			fmt.Println(err)
 		}
